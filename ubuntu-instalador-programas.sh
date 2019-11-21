c0f62ff5223da1f6b73b333e2bee28f57ea92667
Isaias Dimitri <isaiasdimitri2702@gmail.com>
2019-11-20 23:25:28 -0300

    Update ubuntu-instalador-programas.sh

--- ubuntu-instalador-programas.sh
+++ ubuntu-instalador-programas.sh
@@ -4,21 +4,14 @@
 # Obrigado ao Diolinux, por me mostrar como realizar meu "posinstall" via shellscript e ainda deixar tudo bem organizado :) #
 
 # ----------------------------- VARIÁVEIS ----------------------------- #
-PPA_FLATPACK="ppa:alexlarsson/flatpak"
 
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 
 PROGRAMAS_PARA_INSTALAR=(
   snapd
   virtualbox
-  flameshot
-  nemo-dropbox
-  steam-installer
-  steam-devices
-  steam:i386
 )
 
 # ----------------------------- REQUISITOS ----------------------------- #
@@ -32,10 +25,6 @@ sudo dpkg --add-architecture i386
 ## Atualizando o repositório ##
 sudo apt update -y
 
-## Adicionando repositórios de terceiros e suporte ao Flatpack
-
-sudo add-apt-repository "$PPA_FLATPACK" -y
-
 # ----------------------------- EXECUÇÃO ----------------------------- #
 ## Atualizando o repositório depois da adição de novos repositórios ##
 sudo apt update -y
@@ -43,7 +32,6 @@ sudo apt update -y
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SIMPLE_NOTE"         -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
@@ -63,7 +51,6 @@ flatpak install flathub com.obsproject.Studio -y
 ## Instalando pacotes Snap ##
 sudo snap install spotify
 sudo snap install slack --classic
-sudo snap install photogimp
 sudo snap install telegram-desktop
 sudo snap install code --classic
 sudo snap install insomnia
@@ -72,7 +59,6 @@ sudo snap install insomnia
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza ##
 sudo apt update && sudo apt dist-upgrade -y
-flatpak update
 sudo apt autoclean
 sudo apt autoremove -y
 # ---------------------------------------------------------------------- #
